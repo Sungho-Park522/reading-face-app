06f889200c8b4d97db2104324f968315d2d7c67e
Sungho-Park522 <83781351+Sungho-Park522@users.noreply.github.com>
2025-06-12 17:49:48 +0900

    ui/ux 개선중2

--- src/App.js
+++ src/App.js
@@ -47,6 +47,7 @@ if (Object.values(firebaseConfig).every(v => v)) {
 
 
 // 아이콘 정의
+// eslint-disable-next-line no-unused-vars
 const UploadCloudIcon = ({ className }) => (<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className={className}><path d="M4 14.899A7 7 0 1 1 15.71 8h1.79a4.5 4.5 0 0 1 2.5 8.242"></path><path d="M12 12v9"></path><path d="m16 16-4-4-4 4"></path></svg>);
 const HeartIcon = ({ className, filled }) => (<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill={filled ? "currentColor" : "none"} stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className={className}><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg>);
 const UsersIcon = ({ className }) => (<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className={className}><path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M22 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg>);
@@ -279,7 +280,7 @@ const App = () => {
   const [pageState, setPageState] = useState('main');
   const [resultId, setResultId] = useState(null);
 
-  const [showInterstitialAd, setShowInterstitialAd] = useState(false);
+  // const [showInterstitialAd, setShowInterstitialAd] = useState(false);
   const [showResults, setShowResults] = useState(false);
   const [isWatchingRewardedAd, setIsWatchingRewardedAd] = useState(false);
   const [copyStatus, setCopyStatus] = useState('');
